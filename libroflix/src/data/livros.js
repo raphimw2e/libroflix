523f8cfb664992bc244cf4b7f8ac511e3bcd914e
fefter <fernandappsilva02@gmail.com>
2026-03-19 16:46:37 -0300

    organização das páginas e componentes

---
+++ libroflix/src/data/livros.js
@@ -0,0 +1,48 @@
+export const livros = [
+    {
+        id: "1",
+        titulo: "Las Venas Abiertas de América Latina",
+        autor: "Eduardo Galeano",
+        nota: "4.5",
+        duracao: "18h 10min" ,
+        descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde excepturi tempora delectus earum vero quaerat quas consectetur rerum ab veniam quasi quos nihil laboriosam, sed veritatis non labore in. Illo cupiditate minus velit sint. Molestiae pariatur voluptatem libero nisi! Ducimus, voluptates aspernatur! Harum, odio quae?",
+
+
+    },
+    {
+        id: 2,
+        titulo: "El Libro de los Abrazos",
+        autor: "Eduardo Galeano",
+
+        
+    }, 
+    {
+        id: 3,
+        titulo: "El Futbol a sol y sombra",
+        autor: "Eduardo Galeano",
+
+        
+    }, 
+    {
+        id: 4,
+        titulo: "Dias y Noches de Amor y de Guerra",
+        autor: "Eduardo Galeano",
+
+        
+    }, 
+    {
+        id: 5,
+        titulo: "Vagamundo",
+        autor: "Eduardo Galeano",
+
+        
+    }, 
+    {
+        id: 6,
+        titulo: "Patas Arriba",
+        autor: "Eduardo Galeano",
+
+    }
+]
+
+export default livros;
---
+++ libroflix/tailwind.config.js
@@ -0,0 +1,18 @@
+export default { 
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Poltawski Nowy'],
+      },
+      colors: {
+        primary: '#BF2619BF', 
+        'off-white-40%': '#ffffff66', 
+      },
+    },
+  },
+  plugins: [],
+}
