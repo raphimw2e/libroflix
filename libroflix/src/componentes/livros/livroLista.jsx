fb9a5c9c97fb4f4605abb89b808fb5bee4d15ebb
fefter <fernandappsilva02@gmail.com>
2026-03-24 22:01:56 -0300

    Ajustando informações sobre o livro

--- libroflix/src/componentes/livros/livroLista.jsx
+++ libroflix/src/componentes/livros/livroLista.jsx
@@ -1,10 +1,23 @@
 import LivroCard from "./livroCard";
+import { livros } from "libroflix\src\data\livros.js"
+import React from "react";
 
-const LivroLista = ({livros}) => {
+const LivroLista = ({ livro }) => {
+
+    if (!livro) return <p>Carregando os dados do livro...</p>
+
     return (
-        <div className="flex gap-4 mt-4">
-           {livros.map((livro) =>
-                <LivroCard key={livro.id} livro={livro}/>)}
+        <div className=" w-full flex-col flex gap-4 mt-4">
+           <h1 className="text-4xl font-serif font-bold text-book-brown
+            leading-tight uppercase">
+            {livro.titulo}
+           </h1>
+
+           <p className="text-lg font-mediun">
+            Por <span className="underline decoration-1 underline-offset cursor-pointer">
+                {livro.autor}
+            </span>
+           </p>
         </div>
     )
 }
